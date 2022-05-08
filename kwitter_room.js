04da7e19807745339571856343c5bf6d7c74ee35
sharkmaster121 <99085333+sharkmaster121@users.noreply.github.com>
2022-05-08 11:13:48 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,58 @@
+
+const firebaseConfig = {
+      apiKey: "AIzaSyBzSTX5h438wxeF7fWocUp2skeZ0xml6Rg",
+      authDomain: "kwitter-275ed.firebaseapp.com",
+      databaseURL: "https://kwitter-275ed-default-rtdb.firebaseio.com",
+      projectId: "kwitter-275ed",
+      storageBucket: "kwitter-275ed.appspot.com",
+      messagingSenderId: "12715051345",
+      appId: "1:12715051345:web:632538c90a524f59baeb70"
+    };
+    
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+
+user_name = localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+
+function addRoom() {
+
+      room_name = document.getElementById("room_name").value;
+
+      firebase.database().ref("/").child(room_name).update ({
+            purpose : "adding room name"
+      });
+
+      localStorage.setItem("room_name", room_name);
+
+      window.location = "kwitter_page.html";
+  }
+
+function getData() {
+      firebase.database().ref("/").on('value', function (snapshot) {
+            document.getElementById("output").innerHTML = "";
+            snapshot.forEach(function (childSnapshot) {
+                  childKey = childSnapshot.key;
+                  Room_names = childKey;
+                  //Start code
+                  console.log("Room Name - " + Room_names);
+                  row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#></div><hr>";
+                  document.getElementById("output").innerHTML += row;
+                  //End code
+            });
+      });
+}
+getData();
+
+  function redirectToRoomName(name) {
+
+      console.log(name);
+      localStorage.setItem("room_name", name);
+      window.location = "kwitter_page.html";
+  }
+
+  function logout() {
+        localStorage.removeItem("user_name");
+        localStorage.removeItem("room_name");
+        window.location = "index.html";
+  }
